7a9bb2ff5b21876a9b3bdba670bdd2639ab31f16
Luiz Junior <lcj_souza@yahoo.com.br>
2022-06-05 01:40:16 -0300

    RESULTADOS FEITO

--- src/app/resultados/resultados.component.ts
+++ src/app/resultados/resultados.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ResultadosInterface } from './resultados';
 import { ResultadosService } from './resultados.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-resultados',
@@ -7,12 +9,41 @@ import { ResultadosService } from './resultados.service';
   styleUrls: ['./resultados.component.scss']
 })
 export class ResultadosComponent implements OnInit {
+  lotofacil: any;
 
   constructor(public resultados: ResultadosService) { }
 
   ngOnInit(): void {
+    this.ultimoConcurso()
+  }
+
+  ultimoConcurso(){
     this.resultados.getLotofacil()
-    .then(response => console.log(response))
+    .then(response => {
+      this.lotofacil = response;
+      console.log(response);
+      
+    })
+    .catch(error => console.error(error)
+    )
+  }
+
+  buscarConcurso(){
+    const concurso = $('#n-concurso').val();
+    if (concurso === '' || concurso === null) {
+      console.log('Vazio');
+    }else{
+      this.lotofacilConcurso(String(concurso));
+    }
+    
+  }
+
+  lotofacilConcurso(numero: string){
+    this.resultados.getLotofacilSorteio(numero)
+    .then(response => {
+      this.lotofacil = response;
+      console.log(response);
+    })
     .catch(error => console.error(error)
     )
   }
