408ecd11e493d931b1e8390be553e3929d59f5d7
Luiz Junior <lcj_souza@yahoo.com.br>
2022-06-16 03:26:53 -0300

    Layout Desktop e Mascara nos valores

--- src/app/resultados/lotomania/lotomania.component.ts
+++ src/app/resultados/lotomania/lotomania.component.ts
@@ -23,16 +23,20 @@ export class LotomaniaComponent implements OnInit {
     this.resultados
       .getLotomania()
       .then((response) => {
+        this.formatValor(response);
         this.lotomania = response;
         this.spinner = false;
         console.log(response);
       })
       .catch((error) => {
         Swal.fire({
-          title: 'Ops !',
-          text: 'Não foi possível carregar o último sorteio. Tente buscar pelo número do sorteio.',
-          icon: 'error'
-        })
+          title:
+            '<p style="font-size: 2rem; font-weight: 700; color: #000066;">Ops !</p>',
+          html: '<p style="font-size: 1.5rem; font-weight: 700; color: #000066;">Não foi possível carregar o último sorteio. Tente buscar pelo número do sorteio.</p>',
+          icon: 'error',
+          showCloseButton: true,
+          confirmButtonColor: '#F78100',
+        });
         console.error(error);
       });
   }
@@ -40,7 +44,14 @@ export class LotomaniaComponent implements OnInit {
   buscarConcurso() {
     const concurso = $('#n-concurso').val();
     if (concurso === '' || concurso === null) {
-      console.log('Vazio');
+      Swal.fire({
+        title:
+          '<p style="font-size: 2rem; font-weight: 700; color: #000066;">Ops !</p>',
+        html: '<p style="font-size: 1.5rem; font-weight: 700; color: #000066;">Nenhum concurso foi digitado.</p>',
+        icon: 'error',
+        showCloseButton: true,
+        confirmButtonColor: '#F78100',
+      });
     } else {
       this.lotomaniaConcurso(String(concurso));
     }
@@ -51,17 +62,55 @@ export class LotomaniaComponent implements OnInit {
     this.resultados
       .getLotomaniaSorteio(numero)
       .then((response) => {
+        this.formatValor(response);
         this.lotomania = response;
+        $('#n-concurso').val('');
         this.spinner = false;
         console.log(response);
       })
       .catch((error) => {
+        this.spinner = false;
         Swal.fire({
-          title: 'Ops !',
-          text: 'Concurso não encontrado. Verifique o número digitado e tente novamente.',
+          title:
+            '<p style="font-size: 2rem; font-weight: 700; color: #000066;">Ops !</p>',
+          html: '<p style="font-size: 1.5rem; font-weight: 700; color: #000066;">Concurso não encontrado. Verifique o número digitado e tente novamente.</p>',
           icon: 'error',
+          showCloseButton: true,
+          confirmButtonColor: '#F78100',
         });
         console.error(error);
       });
+  }
+
+  formatValor(response: any) {
+
+    for (let i = 0; i < response.listaRateioPremio.length; i++) {
+      response.listaRateioPremio[i].valorPremio = response.listaRateioPremio[i].valorPremio.toLocaleString(
+        'pt-br',
+        { style: 'currency', currency: 'BRL' }
+      );
+    }
+
+    return (      
+      (response.valorArrecadado = response.valorArrecadado.toLocaleString(
+        'pt-br',
+        { style: 'currency', currency: 'BRL' }
+      )),
+      (response.valorAcumuladoProximoConcurso =
+        response.valorAcumuladoProximoConcurso.toLocaleString('pt-br', {
+          style: 'currency',
+          currency: 'BRL',
+        })),
+      (response.valorEstimadoProximoConcurso =
+        response.valorEstimadoProximoConcurso.toLocaleString('pt-br', {
+          style: 'currency',
+          currency: 'BRL',
+        })),
+      (response.valorAcumuladoConcursoEspecial =
+        response.valorAcumuladoConcursoEspecial.toLocaleString('pt-br', {
+          style: 'currency',
+          currency: 'BRL',
+        }))
+    );
   }
 }
